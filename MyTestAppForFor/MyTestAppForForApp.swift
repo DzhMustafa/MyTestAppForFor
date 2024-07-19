5d28a6fb855b81b4e5f556897abfeda1670e70a7
DzhMustafa <dzhmustafa@mail.ru>
2024-07-20 00:19:26 +0300

    Initial Commit

---
+++ MyTestAppForFor/MyTestAppForForApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestAppForForApp.swift
+//  MyTestAppForFor
+//
+//  Created by Джабраилов Мустафа on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyTestAppForForApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
