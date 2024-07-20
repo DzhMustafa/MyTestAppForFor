6f329b47e6b4aad58fff8e23800a93756a070a4e
DzhMustafa <dzhmustafa@mail.ru>
2024-07-20 21:57:27 +0300

    New File

---
+++ MyTestAppForFor/SecondView.swift
@@ -0,0 +1,21 @@
+//
+//  SecondView.swift
+//  MyTestAppForFor
+//
+//  Created by Джабраилов Мустафа on 20.07.2024.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Hello, World!xvf")
+        
+        Text("new")
+        
+    }
+}
+
+#Preview {
+    SecondView()
+}
