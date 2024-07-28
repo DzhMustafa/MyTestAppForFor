8aaa6f50af3c6494b78164ddc0022ffc0762c6f1
DzhMustafa <dzhmustafa@mail.ru>
2024-07-28 23:54:45 +0300

    Commit 1

--- MyTestAppForFor/ContentView.swift
+++ MyTestAppForFor/ContentView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
                 Text("Show")
                 Text("Getinger")
                 Text("Print")
+                
+                Text("1")
             }
             
         }
