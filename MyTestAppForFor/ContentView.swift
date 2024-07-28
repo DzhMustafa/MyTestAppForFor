6ccfa48240d810d21e3f16dc1ec62040a6fcb865
DzhMustafa <dzhmustafa@mail.ru>
2024-07-28 23:18:39 +0300

    Add new screen

--- MyTestAppForFor/ContentView.swift
+++ MyTestAppForFor/ContentView.swift
@@ -9,18 +9,41 @@ import SwiftUI
 import MyLibrary
 
 struct ContentView: View {
-
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Hello, world!")
-            // fix
-            // Hello
+        
+        NavigationStack {
+            NavigationLink {
+                SecondView()
+            } label: {
+                Text("Show")
+            }
+            
         }
-        .padding()
+    }
+}
+
+struct SecondView: View {
+    
+    @State var isPresented = true
+    
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+        }
+        .overlay {
+            if isPresented {
+                CustonAlertView()
+                    .frame(maxWidth: .infinity)
+                    .transition(.move(edge: .leading))
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            isPresented = false
+                        }
+                    }
+            }
+        }
     }
 }
 
